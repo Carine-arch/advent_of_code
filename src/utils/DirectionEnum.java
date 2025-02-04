d5b1800fe352db15da989b00c32bd01486a53142
Carine-arch <cbelaizi@laposte.net>
2025-02-04 14:34:26 +0100

    year-2024-day-6-part 1 and wip part 2

---
+++ src/utils/DirectionEnum.java
@@ -0,0 +1,48 @@
+package utils;
+
+public enum DirectionEnum {
+
+    UP(-1, 0),
+    DOWN(1, 0),
+    LEFT(0, -1),
+    RIGHT(0, 1),
+    UPLEFT(-1, -1),
+    UPRIGHT(-1, 1),
+    DOWNLEFT(1, -1),
+    DOWNRIGHT(1, 1);
+
+    private final int rowDelta;
+    private final int colDelta;
+
+    DirectionEnum(int rowDelta, int colDelta) {
+        this.rowDelta = rowDelta;
+        this.colDelta = colDelta;
+    }
+
+    public int getRowDelta() {
+        return rowDelta;
+    }
+
+    public int getColDelta() {
+        return colDelta;
+    }
+
+    public static DirectionEnum fromDeltas(int rowDelta, int colDelta) {
+        for (DirectionEnum direction : values()) {
+            if (direction.rowDelta == rowDelta && direction.colDelta == colDelta) {
+                return direction;
+            }
+        }
+        return null; // ou throw une exception personnalisée si nécessaire
+    }
+
+    public static DirectionEnum getRightDirection(DirectionEnum previousDirection) {
+        return switch (previousDirection) {
+            case UP -> DirectionEnum.RIGHT;
+            case RIGHT -> DirectionEnum.DOWN;
+            case DOWN -> DirectionEnum.LEFT;
+            case LEFT -> DirectionEnum.UP;
+            default -> throw new IllegalArgumentException("Invalid direction: " + previousDirection);
+        };
+    }
+}
